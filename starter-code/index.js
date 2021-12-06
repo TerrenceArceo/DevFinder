e2c1ceffb10dc8f74cee465644ba95bd2c7db2b2
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2021-12-05 17:10:09 -0800

    Added body's background color, added neccesary font, fixed input field

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,1 @@
-const html = document.getElementById('github-info')
-
-fetch('https://api.github.com/users/TerrenceArceo')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        html.innerHTML = `
-            <a href="${data.html_url}" target="_blank">URL</a>
-        `
-    })
 
