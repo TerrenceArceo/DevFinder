2773f44b85886e809a6e4233572a2c2e920b1aa4
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2021-12-06 22:47:40 -0800

    finished desktop layout for dark theme

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,1 +1,7 @@
+const avatar = document.getElementById('avatar')
 
+fetch('https://api.github.com/users/octocat')
+    .then(res => res.json())
+    .then(data => avatar.innerHTML = `
+        <img src="${data.avatar_url}" alt="avatar">
+    `)
