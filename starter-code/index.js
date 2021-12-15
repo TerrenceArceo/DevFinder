92810fcc5b489cbc8accf9db63891e9c8c4d3c41
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2021-12-14 20:54:19 -0800

    added fetch function to get users and added them on my layout

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,38 @@
 const avatar = document.getElementById('avatar')
+const acc_name = document.getElementById('name')
+const acc_tag = document.getElementById('tag')
+const acc_date = document.getElementById('date-info')
+const bio = document.getElementById('bio')
+const repo = document.getElementById('repo-num')
+const followers = document.getElementById('follower-num')
+const following = document.getElementById('following-num')
+const acc_location = document.getElementById('location')
+const acc_website = document.getElementById('personal-website')
+const twitter = document.getElementById('twitter')
+const company = document.getElementById('company')
 
-fetch('https://api.github.com/users/octocat')
+fetch('https://api.github.com/users/bigdinotech')
     .then(res => res.json())
-    .then(data => avatar.innerHTML = `
-        <img src="${data.avatar_url}" alt="avatar">
-    `)
+    .then(data => {
+        console.log(data)
+        const myDate = new Date(data.created_at)
+        const month = myDate.getMonth()
+        const year = myDate.getFullYear();
+        const day = myDate.getDay();
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", 
+        "September", "October", "November", "December"]
+        
+
+        avatar.innerHTML = `<img src="${data.avatar_url}" alt="avatar">`
+        acc_name.textContent = `${data.name}`
+        acc_tag.textContent = `${data.blog}`
+        acc_date.textContent = `${day} ${months[month]} ${year}`
+        bio.textContent = `${data.bio ? data.bio : "This Profile has no bio"}`
+        repo.textContent = `${data.public_repos}`
+        followers.textContent = `${data.followers}`
+        following.textContent = `${data.following}`
+        acc_location.textContent = `${data.location ? data.location : "Not Available"}`
+        acc_website.textContent = `${data.blog ? data.blog : "Not Available"}`
+        twitter.textContent = `${data.twitter_username ? data.twitter_username : "Not Available"}`
+        company.textContent = `${data.company ? data.company : "Not Available"}`
+    })
